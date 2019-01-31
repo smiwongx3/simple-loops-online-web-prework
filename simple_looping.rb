df5fa72fb72d5bbb84dfd16ef07c178bdde4f0f1
Samantha Wong <smiwong920@gmail.com>
2019-01-31 20:22:43 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,6 +1,7 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
+  number_of_times = 0 
   loop do 
         puts "Welcome to Flatiron School's Web Development Course!"
         break if answer == "STOP"
