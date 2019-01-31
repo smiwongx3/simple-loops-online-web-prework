fa78ab17a79752321569f1236d4700fb070e2a11
Samantha Wong <smiwong920@gmail.com>
2019-01-31 20:26:28 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -4,7 +4,7 @@ def loop_iterator(number_of_times)
   loop do 
     number_of_times += 1 
     puts "Welcome to Flatiron School's Web Development Course!"
-    if number_of_times <= 10 
+    if number_of_times >= 10 
     break
     end 
   end
