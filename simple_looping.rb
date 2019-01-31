84c839756839328c42349deeb8a19f13bfecbc79
Samantha Wong <smiwong920@gmail.com>
2019-01-31 20:23:17 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -4,6 +4,7 @@ def loop_iterator(number_of_times)
   number_of_times = 0 
   loop do 
         puts "Welcome to Flatiron School's Web Development Course!"
+        number_of_times += 1 
         
     end
 end
