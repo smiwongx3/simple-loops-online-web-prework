5d9befe953bf1e84a5aa1f308174445213026b05
Samantha Wong <smiwong920@gmail.com>
2019-01-31 20:32:05 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,6 +1,6 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
-def loop_iterator
+def loop_iterator(number_of_times)
   number_of_times = 0 
   loop do 
     number_of_times += 1 
@@ -11,13 +11,13 @@ def loop_iterator
   end
 end
 
-def times_iterator
+def times_iterator(number_of_times)
   7.times do
 	  puts "Welcome to Flatiron School's Web Development Course!"
 	end
 end
 
-def while_iterator
+def while_iterator(number_of_times)
   counter = 0 
     while counter < 7
 	  puts "Welcome to Flatiron School's Web Development Course!" 
@@ -27,7 +27,7 @@ end
 
 def until_iterator(number_of_times)
   counter = 0
-    until counter == 20
+    until counter == 7
     puts "Welcome to Flatiron School's Web Development Course!"
     counter += 1
   end
